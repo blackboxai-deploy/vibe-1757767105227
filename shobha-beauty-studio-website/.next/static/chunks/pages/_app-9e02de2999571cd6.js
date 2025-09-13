fff34964f3861d504be6844f7c5b3c8abec7c4c8
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-13 18:09:55 +0530

    Add shobha-beauty-studio-website/.next/static/chunks/pages/_app-9e02de2999571cd6.js

---
+++ shobha-beauty-studio-website/.next/static/chunks/pages/_app-9e02de2999571cd6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{2920:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(9444)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(2920),n(5650))),_N_E=_.O()}]);
